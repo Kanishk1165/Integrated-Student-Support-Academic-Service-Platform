f19405a4b161d9823369f69bec78940f77c8dbb5
Nirdesh <kanishkg.2005@gmail.com>
2026-03-07 23:12:48 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     if (mongoose.connection.readyState === 1) return mongoose.connection;
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 20000,
+    });
     console.log(`MongoDB connected: ${conn.connection.host}`);
     return conn.connection;
   } catch (err) {
